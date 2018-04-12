438dd78b8213aeb8fae684f7c850a0ea05344b34
[damienj252] <damienj252@gmail.com>
2018-04-12 17:53:00 +0100

    Skelton of the Project

---
+++ src/main/java/com/ships/G00324923Application.java
@@ -0,0 +1,14 @@
+package com.ships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G00324923Application 
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(G00324923Application.class, args);
+	}//main
+}//G00324923Application
